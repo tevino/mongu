97192d66713b0fba750ee79892f4f6d9b3c0cd43
Tevin Zhang <mail2tevin@gmail.com>
2014-04-18 23:01:04 +0800

    add meta data

--- mongu.py
+++ mongu.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
+"""
+**Mongu** is yet another Python Object-Document Mapper on top of ``PyMongo``. It's lightweight, intuitive to use and easy to understand.
+
+- If you want **control**.
+- If you really care about **performance**.
+- If those **heavy and slow layers** have nothing or more than you need.
+
+Well, **Mongu** maybe the one for you.
+
+You are the only one who knows what you reall need.
+
+Therefor **Mongu** does nothing but a skeleton for you to fill.
+
+Actually, if you have ever tried to write your own ODM, you may already implemented parts of **Mongu** :D
+
+Copyright (c) 2014, Tevin Zhang.
+License: MIT (see LICENSE for details)
+"""
+
+__author__ = 'Tevin Zhang'
+__version__ = '0.1'
+__license__ = 'MIT'
+
+
 import logging
 from bson import ObjectId
 from pymongo import MongoClient
