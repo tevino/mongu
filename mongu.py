f9e56e47dfce137cc01039c74f8fef60907fa809
Tevin Zhang <mail2tevin@gmail.com>
2015-03-17 16:12:08 +0800

    version bump

--- mongu.py
+++ mongu.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.4.2'
+__version__ = '0.4.4'
 
 import logging
 import warnings
--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@
 """
 **Mongu** is yet another Python Object-Document Mapper on top of ``PyMongo``. It's lightweight, intuitive to use and easy to understand.
 
-- If you want **control**.
-- If you really care about **performance**.
-- If those **heavy and slow layers** have nothing or more than you need.
-
-Well, **Mongu** maybe the one for you.
+If those **heavy and slow layers** have nothing or more than you need, Mongu maybe the one for you.
 
 You are the only one who knows what you reall need.
 
-Therefor **Mongu** does nothing but a skeleton for you to fill.
+Therefor **Mongu does nothing but a skeleton for you to fill**.
 
 Actually, if you have ever tried to write your own ODM, you may already implemented parts of **Mongu** :D
 
@@ -19,7 +15,7 @@ License: MIT (see LICENSE for details)
 """
 
 __author__ = 'Tevin Zhang'
-__version__ = '0.4.2'
+__version__ = '0.4.4'
 __license__ = 'MIT'
 
 from setuptools import setup
